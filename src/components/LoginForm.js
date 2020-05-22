db6c9dce6923316aaac9310dd55a5a13d1a4fc1e
Glitch (starter-react-template) <none>
2020-05-22 21:59:24 +0700

    done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,15 +6,16 @@ class LoginForm extends Component {
       <div className="container">
         <div className="header">
           <div className="go-back">
-            <img />
-            <p>Go back</p>
+            <i class="fas fa-long-arrow-alt-left"></i>
+            <span>
+              <p>Go back</p>
+            </span>
           </div>
 
-          <img className="logo" src="https://www.coderx.co/img/logo-dark.png"/>
-          
+          <img className="logo" src="https://www.coderx.co/img/logo-dark.png" />
 
           <div className="sign-up">
-            <p>Sign up</p>
+            <span>Sign up</span>
           </div>
         </div>
 
@@ -22,23 +23,31 @@ class LoginForm extends Component {
           <h1 className="title">Sign in</h1>
 
           <form>
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" />
+            <div className="username">
+              <label for="username">Username</label>
+              <input type="text" id="username" name="username" value="Thanh Lam"/>
+            </div>
 
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" />
+            <div className="username">
+              <label for="password">Password</label>
+              <input type="password" id="password" name="password" value="123123z"/>
+            </div>
 
-            <input type="submit" value="Submit" className="submit-btn" />
+            <input type="submit" value="Sign In" className="submit-btn" />
           </form>
 
           <div className="keep-sign-in">
             <input type="checkbox" value="keep-sign-in" className="check-box" />
-            <p>Keep me signed in</p>
+            <span>Keep me signed in</span>
           </div>
-          
+
           <div className="forgot">
-            <a href="#" className="userName">Forgot username?</a>
-            <a href="#" className="password">Forgot password?</a>
+            <a href="#" className="userName">
+              Forgot username?
+            </a>
+            <a href="#" className="password">
+              Forgot password?
+            </a>
           </div>
         </div>
       </div>
