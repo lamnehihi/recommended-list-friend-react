46d46f31a20e6dad76306e41ad882a4f29a55723
Glitch (starter-react-template) <none>
2020-05-22 12:26:29 +0000

    👛🍎 Checkpoint ./src/App.css:15678430/491 ./src/App.js:15678430/466 ./src/components/LoginForm.js:15678430/747 ./src/index.css:15678430/6

---
+++ src/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="container">
+        
+        <div className="header">
+          <div className="go-back">
+            
+          </div>
+          
+          <img className="logo"/>
+          
+          <div className="sign-up">
+            
+          </div>
+        </div>
+        
+        <div className="form">
+          <h1 className="title">Sign in</h1>
+          
+          
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
