200471085d43c8bc526c2c793e8d59e33f39c15e
Glitch (starter-react-template) <none>
2020-05-22 12:38:49 +0000

    🐯🎨 Checkpoint ./src/components/LoginForm.js:15678430/4600 ./src/App.js:15678430/73

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <LoginForm />
       </div>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LoginForm extends Component {
   render() {
     return (
       <div className="container">
-        
         <div className="header">
-          <div className="go-back">
-            
-          </div>
-          
-          <img className="logo"/>
-          
-          <div className="sign-up">
-            
-          </div>
+          <div className="go-back"></div>
+
+          <img className="logo" />
+
+          <div className="sign-up"></div>
         </div>
-        
+
         <div className="form">
           <h1 className="title">Sign in</h1>
+
+          <form>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" />
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" />
+
+            <input type="submit" value="Submit" className="submit-btn" />
+          </form>
+
+          <div className="keep-sign-in">
+            <input type="checkbox" value="keep-sign-in" className="check-box" />
+            <p>Keep me signed in</p>
+          </div>
           
-          
+          <div className="forgot">
+            <a href="#" className="userName">Forgot username?</a>
+            <a href="#" className="password">Forgot password?</a>
+          </div>
         </div>
-        
       </div>
     );
   }
